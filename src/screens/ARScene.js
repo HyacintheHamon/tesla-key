c0842c4fcddd2a04e819d93a31a860380742411e
HyacintheHamon <hamon.hyacinthe@gmail.com>
2019-08-11 22:07:59 +0200

    Added view on AR Scene

--- src/screens/ARScene.js
+++ src/screens/ARScene.js
@@ -1,9 +1,16 @@
 'use strict';
 
 import React, { Component } from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Dimensions
+} from 'react-native';
 import { ViroSceneNavigator, ViroARSceneNavigator } from 'react-viro';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
+const { height, width } = Dimensions.get('window');
 var InitialARScene = require('./InitialScene');
 
 class ARScene extends React.Component {
@@ -20,12 +27,50 @@ class ARScene extends React.Component {
 
   render() {
     return (
-      //husk API key
-      <ViroARSceneNavigator {...this.state.sharedProps}
-      initialScene={{scene: InitialARScene}} />
+      <View style={styles.viroContainer} >
+      <ViroARSceneNavigator style={{flex : 1}} {...this.state.sharedProps}
+        initialScene={{scene: InitialARScene}} />
+      <View style={styles.bottomView}>
+        <TouchableOpacity style={styles.closeButton} onPress={this.props.onCloseMap}>
+            <Icon name="times" size={60} color="#fff" />
+        </TouchableOpacity>   
+      </View>
+    </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  viroContainer :{
+    flex : 1,
+    backgroundColor: "#111117", 
+  },
+  buttonText: {
+    color:'#fff',
+    textAlign:'center',
+    fontSize : 20
+  },
+  bottomView: {
+    width: '100%', 
+    height: height/3,
+    backgroundColor: "#111117",
+    margin: 0,
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 0
+  },  
+  closeButton : {
+    position: "relative",
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius:100,
+    width:60,
+    width:60,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 
 module.exports = ARScene;
--- src/screens/InitialScene.js
+++ src/screens/InitialScene.js
@@ -1,15 +1,16 @@
 'use strict';
 
 import React, { Component } from 'react';
-
-import {StyleSheet} from 'react-native';
-
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import {
   ViroARScene,
   ViroText,
+  ViroImage,
   ViroBox,
   ViroMaterials,
 } from 'react-viro';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { View } from 'react-native-animatable';
 
 export default class ARScene extends Component {
 
@@ -27,9 +28,22 @@ export default class ARScene extends Component {
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-      </ViroARScene>
+         /*
+          <TouchableOpacity style={styles.closeButton} onPress={this.props.onCloseMap}>
+            <Icon name="times" size={30} color="#fff" />
+          </TouchableOpacity>   
+        */
+         <ViroARScene onTrackingUpdated={this._onInitialized} >
+        {/* <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} /> */}
+        <ViroImage
+          height={0.2}
+          width={0.2}
+          scale={[.5, .5, .5]} 
+          position={[0, 0, -1]} 
+          source={require("../img/tesla-ar-marker.png")}
+          // animation={{ name: "animateImage", run: this.state.runAnimateImage }}
+        />
+      </ViroARScene>     
     );
   }
 
@@ -49,6 +63,11 @@ var styles = StyleSheet.create({
     textAlignVertical: 'center',
     textAlign: 'center',  
   },
+  closeButton: {
+    position: 'absolute',
+    right: 9,
+    top: 10
+},
 });
 
 module.exports = ARScene;
