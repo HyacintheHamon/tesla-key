d65c99f96b10be5d13cab6c0461290c1498141c0
Hyacinthe Hamon <hamon.hyacinthe@gmail.com>
2019-08-14 15:41:29 +0200

    config for viroReact library on Android

--- android/app/src/main/java/com/tesla/MainApplication.java
+++ android/app/src/main/java/com/tesla/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.viromedia.bridge.ReactViroPackage;
 
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
+      new ReactViroPackage(ReactViroPackage.ViroPlatform.OVR_MOBILE);
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
