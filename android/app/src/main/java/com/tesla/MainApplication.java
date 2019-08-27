4b7591b3c9b0ee74b6f04c1cadcaf1b510734416
Hyacinthe Hamon <hamon.hyacinthe@gmail.com>
2019-08-27 15:09:12 +0200

    Android AR fix

--- android/app/src/main/java/com/tesla/MainApplication.java
+++ android/app/src/main/java/com/tesla/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      new ReactViroPackage(ReactViroPackage.ViroPlatform.OVR_MOBILE);
+      packages.add(new ReactViroPackage(ReactViroPackage.ViroPlatform.OVR_MOBILE));
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
