8fcd9f8b46b0fdf4da6ed54fd6117d73c0fe3f2d
--global <great999lancer@yandex.ru>
2019-09-01 07:05:10 +0300

    Added climate screen

--- src/screens/ClimateControlsModalScreen.js
+++ src/screens/ClimateControlsModalScreen.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { StyleSheet, Text, Image, View, TouchableOpacity,SafeAreaView  } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome5';
 import { create, PREDEF_RES } from 'react-native-pixel-perfect';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
-import  { Close } from '../img/svg'
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 perfectSize = create(PREDEF_RES.iphoneX.px);
-const vent = require("../img/vent.png");
-const trunk = require("../img/trunk.png");
-const frunk = require("../img/frunk.png");
 const fan = require("../img/fan.png");
-const panic = require("../img/panic.png");
-const port = require("../img/port.png");
-const seat1 = require("../img/seat1.png");
-const seat2 = require("../img/seat2.png");
 
 export default class ClimateControlsModalScreen extends React.Component {
+
+  constructor(props) {
+		super(props)
+      this.state = {
+        climate: {
+          auto: false,
+          ac: false,
+          powerOn: false,
+          turnFace: false,
+          velocity: 2
+        }
+      };
+  }
 
   setNavigationColor = (color) => {
     changeNavigationBarColor(color);
@@ -25,104 +29,81 @@ export default class ClimateControlsModalScreen extends React.Component {
     this.setNavigationColor('#111117');
   }
 
+  increaseVelocity = () => {
+    this.setState({climate: {...this.state.climate, velocity: ++this.state.climate.velocity}});
+  }
+
+  decreaseVelocity = () => {
+    if (this.state.climate.velocity>0) {
+      this.setState({climate: {...this.state.climate, velocity: --this.state.climate.velocity}});
+    }
+  }
+
   render() {
+    const {climate} = this.state;
+    const {auto, ac, turnFace, powerOn, velocity} = climate;
     return (
       <SafeAreaView style={{flex:1}}>
-      <View style={styles.modalContent}>
-        <Text style={styles.header}>CLIMATE CONTROLS</Text>
-        <TouchableOpacity style={styles.closeButton} onPress={this.props.onCloseClimateControlsModal}>
-          <Close />
+        <TouchableOpacity  onPress={this.props.onCloseClimateControlsModal} style={{...styles.modalContent}}>
+          <View style={{flex: 0.05, justifyContent: 'center'}}>
+            <Text style={styles.header}>CLIMATE CONTROLS</Text>
+          </View>
+          <View style={{flex: 0.3, justifyContent: 'center'}}>
+            <Image source={fan} style={{width: 80, height: 80}}/> 
+          </View>
+          <View style={{flex: 0.65}}>
+            <View style={styles.buttonGroup}>
+              <View style={styles.buttonRow}>
+                <View style={styles.buttonCol}>
+                  <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
+                    <Image style={styles.seatIcon} source={seat1} />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
+                    <Image style={styles.seatIcon} source={seat1} />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
+                    <Image style={styles.seatIcon} source={seat1} />
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.buttonCol}>
+                  <TouchableOpacity style={auto?styles.activeBtn:styles.button} onPress={()=>this.setState({climate: {...climate, auto: !auto}})}>
+                    <Text style={{fontSize: 20, color: 'white'}}>AUTO</Text>                  
+                  </TouchableOpacity>
+                  <View style={styles.longButton}>
+                    <TouchableOpacity style={styles.volButton} onPress={this.increaseVelocity}>
+                      <Text style={styles.buttonText}>+</Text>
+                    </TouchableOpacity>
+                    <Text style={{color: 'white', fontSize: 27}}>{velocity}</Text>
+                    <TouchableOpacity style={styles.volButton} onPress={this.decreaseVelocity}>
+                      <Text style={styles.buttonText}>-</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <View style={styles.buttonCol}>
+                  <TouchableOpacity style={ac?styles.activeBtn:styles.button} onPress={()=>this.setState({climate: {...climate, ac: !ac}})}>
+                    <Text style={{fontSize: 20, color: 'white'}}>A/C</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={turnFace?styles.activeBtn:styles.button} onPress={()=>this.setState({climate: {...climate, turnFace: !turnFace}})}>
+                    <Image style={styles.seatIcon} source={seat1} />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
+                    <Image style={styles.seatIcon} source={seat1} />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.buttonRow}>
+                <TouchableOpacity style={powerOn?styles.activeBtn:styles.button} onPress={()=>this.setState({climate: {...climate, powerOn: !powerOn}})}>
+                  <Ionicons
+                    name={'md-power'}
+                    style={styles.buttonText}
+                    color={'white'}
+                    size={24}
+                  />   
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
         </TouchableOpacity>
-
-        <Image source={fan} style={{width: 80, height: 80}}/> 
-
-        <View style={styles.bottomModal}>
-        <View style={styles.buttonGroup}>
-          <View style={styles.buttonRow}>
-            <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
-                <Image style={styles.seatIcon} source={seat1} />
-              </TouchableOpacity>
-
-            <View style={styles.ctrlTem}>
-              <TouchableOpacity style={styles.button} onPress={this.incrementFirstItem}>
-                <Icon
-                  name={'chevron-up'}
-                  type={'entypo'}
-                  color={'white'}
-                  size={24}
-                />
-              </TouchableOpacity>
-              <Text style={styles.temText}>72</Text>
-              <TouchableOpacity style={styles.button} onPress={this.decrementFirstItem}>
-                <Icon
-                  name={'chevron-down'}
-                  type={'entypo'}
-                  color={'white'}
-                  size={24}
-                />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.item}>
-              <TouchableOpacity style={styles.fanButton} 
-              onLongPress={this.handlerfanButtonLongPress}
-              onPress={this.handlerfanButtonPress}
-              >
-                <Image style={styles.buttonIcon} source={fan} />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.ctrlTem}>
-              <TouchableOpacity style={styles.button} onPress={this.incrementSecondItem}>
-                <Icon
-                  name={'chevron-up'}
-                  type={'entypo'}
-                  color={'white'}
-                  size={24}
-                />
-              </TouchableOpacity>
-              <Text style={styles.temText}>72</Text>
-              <TouchableOpacity style={styles.button} onPress={this.decrementSecondItem}>
-                <Icon
-                  name={'chevron-down'}
-                  type={'entypo'}
-                  color={'white'}
-                  size={24}
-                />
-              </TouchableOpacity>
-            </View>
-            <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
-              <Image style={styles.seatIcon} source={seat2} />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.buttonRow}>
-            <View style={styles.item}>
-              <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
-                <Image style={styles.buttonIcon} source={port} />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.item}>
-              <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
-                <Image style={styles.buttonIcon} source={frunk} />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.item}>
-              <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
-                <Image style={styles.buttonIcon} source={panic} />              
-              </TouchableOpacity>
-            </View>
-            <View style={styles.item}>
-              <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
-                <Image style={styles.buttonIcon} source={trunk} />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.item}>
-              <TouchableOpacity style={styles.button} onPress={()=>alert('button')}>
-                <Image style={styles.buttonIcon} source={vent} />
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </View>
-      </View>
     </SafeAreaView>
     )
   }
@@ -134,14 +115,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#111117',
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'column',
   },
   header:{
-      position: 'absolute',
-      top: 20,
       color: "#fff",
       textTransform: 'uppercase',
       fontFamily: 'Montserrat-Medium',
-      fontSize: 15
+      fontSize: 15,
   },
   closeButton: {
       position: 'absolute',
@@ -154,48 +134,70 @@ const styles = StyleSheet.create({
     justifyContent: 'center', 
     alignItems: 'center',
     position: 'absolute',
-    bottom: 0
+    bottom: 0,
+    flex: 0.7,
   },
   buttonGroup: {
-    flex: 1, 
     flexDirection: 'column',
     width: "90%",
-    marginBottom:  30
   },
   buttonRow: {
-    height: 80,
     flexDirection: 'row',
     flexWrap: "wrap",
     justifyContent: "space-evenly",
-    alignContent: "center"
+    alignContent: "center",
+    width: '100%'
+  },
+  buttonCol: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    margin: 5
   },
   button: {
     alignItems: 'center',
     justifyContent: 'center',
+    height: 70,
+    width: 70,
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 35,
+    marginTop: 17
   },
-  fanButton: {
+  activeBtn: {
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 30
+    height: 70,
+    width: 70,
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 35,
+    marginTop: 17,
+    backgroundColor: 'rgba(10, 120, 233, 0.9)'
   },
-  buttonIcon: {
-    width: perfectSize(80),
-    height: perfectSize(80)
+  volButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    width: 70
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 40
+  },
+  longButton: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    height: 160,
+    width: 70,
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 35,
+    marginTop: 17,
+    flexDirection: 'column'
   },
   seatIcon: {
     width: perfectSize(80),
     height: perfectSize(150)
-  },
-  temText: {
-    color: 'white',
-    width: '100%',
-    justifyContent: 'center',
-    fontSize: 24
-  },
-  ctrlTem: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    margin: 5
   }
 })
 
