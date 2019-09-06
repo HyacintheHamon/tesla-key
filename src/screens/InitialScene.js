2d581e8cd53a5773b6e6c6c209c4799e87e9b7a9
HyacintheHamon <hamon.hyacinthe@gmail.com>
2019-09-06 19:39:19 +0200

    tweak for  mercator

--- src/screens/InitialScene.js
+++ src/screens/InitialScene.js
@@ -28,6 +28,8 @@ export default class ARScene extends Component {
         latitude: 0.0,
         longitude: 0.0,
       },
+      imageFinalPosX: 0,
+      imageFinalPosZ: 0
     };
 
     // bind 'this' to functions
@@ -64,17 +66,40 @@ export default class ARScene extends Component {
         var currentDevicePos = merc.fromLatLngToPoint({lat: currentDeviceLat, lng: currentDeviceLong});
 
         var imageFinalPosX = imagePos.x - currentDevicePos.x;
-        var imageFinalPosY = imagePos.y - currentDevicePos.y;
+        var imageFinalPosZ = imagePos.y - currentDevicePos.y;
 
-        console.log("image final position X: ", imageFinalPosX);
-        console.log("image final position Y: ", imageFinalPosY);
+        console.log("image final position X :", imageFinalPosX);
+        console.log("image final position Z : ", imageFinalPosZ);
+
+        // Since the position has to be in xyz, assuming that height is 1m
+        // x = imageFinalPosX
+        // y = 1
+        // z = imageFinalPosZ
+
+        // Rotate the final positions
+        // var angle = heading;
+        // var newRotatedX = imageFinalPosX * Math.cos(angle) - imageFinalPosZ * Math.sin(angle);
+        // var newRotatedZ = imageFinalPosZ * Math.cos(angle) + imageFinalPosX * Math.sin(angle);
+
+        this.setState({
+          currentPosition: {
+              imageFinalPosX: imageFinalPosX,
+              imageFinalPosZ: imageFinalPosZ
+          }
+        });
+
       },
       (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
   }
 
   render() {
+    const { imageFinalPosX, imageFinalPosZ } = this.state;
+
+    console.log("image final position X in render method: ", imageFinalPosX);
+    console.log("image final position Z in render method: ", imageFinalPosZ);
+
     return (
          /*
           <TouchableOpacity style={styles.closeButton} onPress={this.props.onCloseMap}>
@@ -88,6 +113,7 @@ export default class ARScene extends Component {
           width={0.2}
           scale={[.5, .5, .5]} 
           position={[0, 0, -1]} 
+          // position={[imageFinalPosX, 1, imageFinalPosZ]} 
           source={TeslaMarker}
           // animation={{ name: "animateImage", run: this.state.runAnimateImage }}
         />
